52095eed54996def885e041c837d64b36103b83b
LakshanJayalath171 <lakshanjayalath171@gmail.com>
2026-05-01 00:44:28 +0530

    All blog page implementation successful

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Homepage from "./Pages/Homepage"
 import Login from "./Pages/Login"
 import BlogPage from "./Pages/BlogPage"
+import AllBlogs from "./Pages/AllBlogs"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
           <Route path="/" element={<Homepage/>}/>
           <Route path="/admin/login" element={<Login/>}/>
           <Route path='/blog/:id' element={<BlogPage/>}/>
+          <Route path="/blog/all" element={<AllBlogs/>}/>
         </Routes>
     </div>
   )
---
+++ src/Pages/AllBlogs.tsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import { hero, sample_blogs } from "../assets/assest"
+import { logo } from "../assets/assest"
+import { IoArrowBackCircleSharp } from "react-icons/io5";
+import BlogCard from "../Components/BlogCard";
+import SmallFooter from "../Components/SmallFooter";
+
+const AllBlogs = () => {
+  return (
+    <div>
+        {/* hero section  */}
+        <div style={{backgroundImage:`url(${hero})`}} className="w-screen h-screen bg-cover bg-center relative">
+            <div className="w-screen absolute -top-5 left-0 px-8 flex items-center justify-between gap-2">
+                <Link to={"/"}>
+                  <IoArrowBackCircleSharp className="text-4xl text-white "/>
+                </Link>
+                <img src={logo} alt="learn stack logo" className="w-28 h-28"/>
+            </div>
+        </div>
+
+        {/* blog title section  */}
+
+        <div className="flex items-center justify-center px-10 py-4">
+            <h1 className="text-6xl poppins-bold">All Blogs</h1>
+        </div>
+
+        {/* blog section  */}
+
+        <div className="w-full px-8 grid grid-cols-3">
+            {sample_blogs.map((items,index)=>(
+                <BlogCard title={items.title} image={items.image} _id={items._id}/>
+            ))}
+        </div>
+
+        <SmallFooter/>
+    </div>
+  )
+}
+
+export default AllBlogs
