b85f0cad5fd12bcabd3574ab61b481bba2f239c5
LakshanJayalath171 <lakshanjayalath171@gmail.com>
2026-05-15 16:40:25 +0530

    added backend for the add blog

--- src/Components/AddItem.tsx
+++ src/Components/AddItem.tsx
@@ -3,9 +3,17 @@ import { IoIosAddCircle } from "react-icons/io";
 import "quill/dist/quill.snow.css";
 import Quill from "quill";
 import { blogCategories, uploadImg } from "../assets/assest";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
 
 
 const AddItem = () => {
+
+  const {axios} = useAppContext()
+
+  const [title,setTitle] = useState("")
+  const [category , setCategory] = useState()
+  const [isPublished , setIsPublished] = useState(true)
   const [value, setValue] = useState("");
 
   const editorRef = useRef(null)
@@ -15,11 +23,45 @@ const AddItem = () => {
     setValue(content);
   };
 
+  const addBlog = async ()=>{
+    try {
+      const blogData = {
+        title,
+        content:value,
+        category,
+        isPublished,
+        upVotes:0,
+        downVotes:0,
+        views:0,
+      }
+
+      const {data} = await axios.post('/api/blog/addBlog',blogData)
+
+      if(data.success){
+        toast.success(data.message)
+
+        setTitle("")
+        setValue("")
+        quillRef.current.root.innerHTML = ""
+      }
+      else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   useEffect(()=>{
     if(!quillRef.current && editorRef.current){
       quillRef.current = new Quill(editorRef.current,{theme:"snow"})
     }
+
+    quillRef.current.on("text-change",()=>{
+      setValue(quillRef.current.root.innerHTML)
+    })
   },[])
+
 
   return (
     <div className="flex items-center ">
@@ -30,6 +72,8 @@ const AddItem = () => {
             type="text"
             placeholder="Enter blog title"
             className="p-2 poppins-semibold border border-black mt-2"
+            value={title}
+            onChange={(e)=>setTitle(e.target.value)}
           />
         </div>
 
@@ -51,13 +95,13 @@ const AddItem = () => {
 
         <div>
           <h2 className="poppins-bold">Category</h2>
-          <select name="category" id="" className="px-6 py-2 poppins-semibold border border-balck m-3">
+          <select name="category" value={category} onChange={(e)=>setCategory(e.target.value)} id="" className="px-6 py-2 poppins-semibold border border-balck m-3">
             {blogCategories.map((items,index)=>(
               <option value={items.name} key={index}>{items.name}</option>
             ))}
           </select>
         </div>
-        <button type="submit" className="px-10 py-2 rounded-3xl bg-blue-500 flex items-center justify-center gap-2 cursor-pointer">Add Blog<IoIosAddCircle/></button>
+        <button onClick={addBlog} type="submit" className="px-10 py-2 rounded-3xl bg-blue-500 flex items-center justify-center gap-2 cursor-pointer">Add Blog<IoIosAddCircle/></button>
       </div>
     </div>
   );
